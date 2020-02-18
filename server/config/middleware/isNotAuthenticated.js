209ab68791e8335dcb319ef91a7907a853f8992c
CalumR23 <searagan@gmail.com>
2020-02-18 12:32:02 -0800

    added server boilerplate, will test functionality after frontend is complete

---
+++ server/config/middleware/isNotAuthenticated.js
@@ -0,0 +1,10 @@
+// This is middleware for restricting routes a user is not allowed to visit if not logged in
+module.exports = function(req, res, next) {
+    // If the user isn't logged in, continue with the request to the restricted route
+    if (!req.user) {
+      return next();
+    }
+  
+    // If the user is logged in, redirect them to the home page
+    return res.redirect("/");
+  };
